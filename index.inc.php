82758a419b797cc9427df16a4d1074e393095741
Deepak Singh <deepak2005@gmail.com>
2013-02-19 23:17:43 -0500

    Title Changed

--- index.inc.php
+++ index.inc.php
@@ -12,12 +12,12 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
 <head>
-  <title>PHProxy</title>
+  <title>Visit Website--Deepak Singh</title>
   <link rel="stylesheet" type="text/css" href="style.css" title="Default Theme" media="all" />
 </head>
 <body onload="document.getElementById('address_box').focus()">
 <div id="container">
-  <h1 id="title">PHProxy</h1>
+  <h1 id="title">Website Proxy</h1>
   <ul id="navigation">
     <li><a href="<?php echo $GLOBALS['_script_base'] ?>">URL Form</a></li>
     <li><a href="javascript:alert('cookie managment has not been implemented yet')">Manage Cookies</a></li>
